77823e948ab2ef5373bc66e6a9d1bf494272e612
Moshekwa <malatjim04@gmail.com>
2023-03-11 20:41:15 +0200

    Api key aunthenticated using decorator

--- app.py
+++ app.py
@@ -10,8 +10,8 @@ app.config["API_TITLE"] = "Aunthenticate API"
 app.config["API_VERSION"] = "v1"
 app.config["OPENAPI_VERSION"] = "3.0.3"
 app.config["OPENAPI_URL_PREFIX"] = "/"
-#app.config["OPENAPI_SWAGGER_UI_PATH"] = "/swagger-ui"
-#app.config["OPENAPI_SWAGGER_UI_URL"] = "https://cdn.jsdelivr.net/npm/swagger-ui-dist/"
+app.config["OPENAPI_SWAGGER_UI_PATH"] = "/swagger-ui"
+app.config["OPENAPI_SWAGGER_UI_URL"] = "https://cdn.jsdelivr.net/npm/swagger-ui-dist/"
 
 api = Api(app)
 api.register_blueprint(GetRequestBlueprint)
--- resources/gets.py
+++ resources/gets.py
@@ -1,14 +1,29 @@
-import uuid
 from flask import request
 from flask.views import MethodView
 from flask_smorest import Blueprint
+from functools import wraps
 
-blp = Blueprint("stores", __name__, description="GET Request Operations")
+from CONFIG import key
+
+blp = Blueprint("Get Requests Routes", __name__, description="GET Request Operations")
+
+def authenticate(func):
+    @wraps(func)
+    def wrapper(*args, **kwargs):
+        token = request.headers.get('token')
+
+        if token != key:
+            return {"message":"Unauthorized access"},401
+        else:
+            return func(*args, **kwargs)
+
+    return wrapper
 
 @blp.route("/")
 class Home(MethodView):
+    @authenticate
     def get(self):
         try:
-            return {"Message":"Welcome to Flask API Authentication Project"}
+            return {"Message":"Your are authenticated! :)"}
         except Exception as e:
             return e, 500
